7a6222ca61b1b1a0e24e18e8b15744874932b778
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-30 21:29:53 -0500

    sinc class methods to display headers, params, and session

--- lib/sinc.rb
+++ lib/sinc.rb
@@ -2,4 +2,16 @@ class Sinc
   def self.test_gem
     "sinc"
   end
+
+  def self.print_headers(headers)
+    "Headers: #{headers}"
+  end
+
+  def self.print_params(params)
+    "Params: #{params}"
+  end
+
+  def self.session?(session)
+    "Session?: #{session.any?}"
+  end
 end
--- spec/sinc_spec.rb
+++ spec/sinc_spec.rb
@@ -1,10 +1,28 @@
+ENV['RACK_ENV'] = 'test'
+
 require "spec_helper"
 
 describe "sinc gem" do
+  include Rack::Test::Methods
   require "sinc"
+
+  def app
+    Sinatra::Application
+  end
 
   it "should say hi" do
     expect(Sinc.test_gem).to eq("sinc")
   end
 
+  it "should display the headers" do
+    expect(Sinc.headers({"Content-Type" => "text/plain"})).to eq("Headers: {\"Content-Type\"=>\"text/plain\"}")
+  end
+
+  it "should display the params" do
+    expect(Sinc.params({:fake_key => "fake value"})).to eq("Params: {:fake_key=>\"fake value\"}")
+  end
+
+  it "should let the user know if there is a session present" do
+    expect(Sinc.session?({:fake_user_id => 1})).to eq("Session?: true")
+  end
 end
