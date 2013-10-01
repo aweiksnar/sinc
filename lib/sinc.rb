30d07ce59098211194dc2d4664d00ef9f57f460d
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-30 22:44:00 -0500

    after block formatting

--- lib/sinc.rb
+++ lib/sinc.rb
@@ -21,7 +21,11 @@ class Sinc
 end
 
 after do
-  Sinc.print_data("", Sinc.print_headers(response.headers), Sinc.print_params(params), Sinc.session?(session), "Response:")
+  Sinc.print_data(
+    "",
+    Sinc.print_headers(response.headers),
+    Sinc.print_params(params),
+    Sinc.session?(session), "Response:")
 end
 
 
