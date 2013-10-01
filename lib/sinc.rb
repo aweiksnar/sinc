6688c9698e73ab4d971966d662b54ea608e113e2
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-30 21:54:06 -0500

    add print data method and invoke in after block

--- lib/sinc.rb
+++ lib/sinc.rb
@@ -14,4 +14,14 @@ class Sinc
   def self.session?(session)
     "Session?: #{session.any?}"
   end
+
+  def self.print_data(*args)
+    args.each {|arg| puts arg}
+  end
 end
+
+after do
+  Sinc.print_data("", Sinc.print_headers(response.headers), Sinc.print_params(params), Sinc.session?(session), "Response:")
+end
+
+
--- spec/sinc_spec.rb
+++ spec/sinc_spec.rb
@@ -4,6 +4,7 @@ require "spec_helper"
 
 describe "sinc gem" do
   include Rack::Test::Methods
+  require 'rspec/mocks/standalone'
   require "sinc"
 
   def app
@@ -15,14 +16,25 @@ describe "sinc gem" do
   end
 
   it "should display the headers" do
-    expect(Sinc.headers({"Content-Type" => "text/plain"})).to eq("Headers: {\"Content-Type\"=>\"text/plain\"}")
+    expect(Sinc.print_headers({"Content-Type" => "text/plain"})).to eq("Headers: {\"Content-Type\"=>\"text/plain\"}")
   end
 
   it "should display the params" do
-    expect(Sinc.params({:fake_key => "fake value"})).to eq("Params: {:fake_key=>\"fake value\"}")
+    expect(Sinc.print_params({:fake_key => "fake value"})).to eq("Params: {:fake_key=>\"fake value\"}")
   end
 
   it "should let the user know if there is a session present" do
     expect(Sinc.session?({:fake_user_id => 1})).to eq("Session?: true")
+  end
+
+  describe "printing to the console" do
+    before do
+      Sinc.stub(:print)
+      Sinc.stub(:puts)
+    end
+
+    it "should have a method that displays a list of items" do
+      expect(Sinc.print_data("Hello", "World")).to eq(["Hello", "World"])
+    end
   end
 end
