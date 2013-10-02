9782c850fd6964dc785947bdcadcc6675b2f5a7b
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-02 11:46:28 -0500

    dry up file requirements

--- lib/sinc.rb
+++ lib/sinc.rb
@@ -1,3 +1,2 @@
 require "after_block.rb"
 require "sinc.rb"
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "rspec"
 require "rack/test"
-Dir["./lib/**/*.rb"].each {|file| require file }
+Dir["./lib/*/*.rb"].each {|file| require file }
 
 RSpec.configure do |config|
   config.color_enabled = true
